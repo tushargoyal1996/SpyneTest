e0b461ff51260e67918174cd738b15b21bebc1d0
Tushar Goyal <tushar.goyal@remotestate.com>
2022-04-27 15:05:28 +0530

    Initial project setup

---
+++ SypneTest/SypneTest.h
@@ -0,0 +1,18 @@
+//
+//  SypneTest.h
+//  SypneTest
+//
+//  Created by RemoteState on 27/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SypneTest.
+FOUNDATION_EXPORT double SypneTestVersionNumber;
+
+//! Project version string for SypneTest.
+FOUNDATION_EXPORT const unsigned char SypneTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SypneTest/PublicHeader.h>
+
+
